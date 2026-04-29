4302a740a8a2a9cdd59b747ac60ac37ffeb1e185
Geo <geoobero@gmail.com>
2026-04-29 14:02:10 +0800

    footer text sizes

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -8,10 +8,10 @@ export default function Footer() {
                     <h2 className="mb-4 text-2xl font-bold">Stay Updated</h2>
                     <p className="mb-6 opacity-90">Subscribe to our newsletter for the latest products and deals</p>
                 </div>
-                <div className="mx-auto text-center lg:ml-10 lg:text-start">
-                    Photo used from{' '}
-                    <a className="underline" target="_blank" rel="noreferrer" href="https://unsplash.com">
-                        Unsplash.com
+                <div className="mx-auto text-sm text-center lg:ml-10 lg:text-start">
+                    Photo used with permission from{' '}
+                    <a className="" target="_blank" rel="noreferrer" href="https://unsplash.com">
+                        <span className="text-amber-500">Unsplash</span>
                     </a>
                 </div>
             </section>
